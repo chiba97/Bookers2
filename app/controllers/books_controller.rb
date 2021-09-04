6ba2799cd825b1cff7390529e1f66808d40ffcf1
chiba97 <ktfkyrya@gmail.com>
2021-09-04 05:17:35 +0000

    [Add]view-page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,23 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
   
   def show
+    @book_ = Book.new
+    @book = Book.find(params[:id])
+    @user = current_user
   end
   
   def edit
@@ -17,4 +28,10 @@ class BooksController < ApplicationController
   
   def destroy
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
   end
   
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
   
   def edit
