f603e0fae51840045b8a159397d4ade69dc238d3
chiba97 <ktfkyrya@gmail.com>
2021-09-05 07:13:43 +0000

    コード修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "about" => "homes#about"
-  resources :books
+  resources :books,only:[:create,:index,:show,:edit,:update,:destroy]
   resources :users,only:[:index,:show,:edit,:update]
 end
